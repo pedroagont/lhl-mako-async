9968900227bd8369c2fb4517e8a22cdb7c54caf8
Pedro A. González <pgonzalez@post.com>
2023-11-10 12:53:17 -0600

    First commit, add sync vs async example

---
+++ sync-vs-async.js
@@ -0,0 +1,38 @@
+// SYNC
+// Sync simple
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+// Sync with time/resources consuming task
+// console.log(1);
+// for (let i = 0; i < 10000000000; i++) {
+//   //
+// }
+// console.log(2);
+// console.log(3);
+
+// ASYNC
+// Async anonymous
+// console.log(1);
+// setTimeout(() => {
+//   for (let i = 0; i < 10000000000; i++) {
+//     //
+//   }
+//   console.log(2);
+// }, 1000);
+// console.log(3);
+
+// // Async not anonymous
+// const handleTimeout = () => {
+//   for (let i = 0; i < 10000000000; i++) {
+//     //
+//   }
+//   console.log(2);
+// };
+// console.log(1);
+// setTimeout(handleTimeout, 1000);
+// console.log(3);
+
+// // Other async example
+// setInterval(() => console.log('Hello!'), 1000);
